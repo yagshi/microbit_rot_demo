a293c84882afaa5aedfda28c1483fecebeef8c0b
yagshi <ugsf@outlook.com>
2021-05-09 10:58:24 +0900

    mini optimization

--- main.ts
+++ main.ts
@@ -13,10 +13,11 @@ function readPat() {
 
 function rotate(pat: number[][], angle: number) {
     const ret: number[][] = [[], [], [], [], [], [], []];
+    const s = Math.sin(angle * Math.PI / 180);
+    const c = Math.cos(angle * Math.PI / 180);
+    
     for (let y = 1; y < 6; y++) {
         for (let x = 1; x < 6; x++) {
-            const s = Math.sin(angle);
-            const c = Math.cos(angle);
             const x1 = c * (x - 3) - s * (y - 3) + 3;
             const y1 = s * (x - 3) + c * (y - 3) + 3;
             const x1i = Math.floor(x1);
@@ -31,12 +32,14 @@ function rotate(pat: number[][], angle: number) {
     return ret;
 }
 const tgt: string = "/._0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+
 basic.forever(function () {
     for (let i = 0; i < tgt.length; i++) {
         basic.showString(tgt[i]);
         const pat0 = readPat();
         for (let angle = 0; angle < 720; angle += 5) {
-	        const pat = rotate(pat0, Math.PI * angle / 180);
+	        const pat = rotate(pat0, angle);
             for (let y = 0; y < 5; y++) {
                 for (let x = 0; x < 5; x++) {
                     led.plotBrightness(x, y, pat[y + 1][x + 1]);
